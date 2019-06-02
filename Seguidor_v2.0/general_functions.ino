5a6eaf9628b3a566d99cc0e708f28c5ebb187b8b
Wellington <montagnini@alunos.utfpr.edu.br>
2019-06-02 20:41:46 -0300

    Update general_functions.ino

--- Seguidor_v2.0/general_functions.ino
+++ Seguidor_v2.0/general_functions.ino
@@ -6,40 +6,54 @@ void ler_sensores (void) {
   leitura[4] = digitalRead(s4);
 }
 
-void definir_acao (int leitura[]){
-  if (leitura[4] == 1 && andar == 0){
-    andar = 1;
-    modo = andando;
+
+void definir_acao ( void ){
+  //Sensor de início/fim ativo.
+  if(leitura[4] == 1){
+    //Inicio de corrid(sensor de inicio/fim ativo e carro parado).
+      if (andar == 0){
+        andar = 1; 
+        modo = andando;
+      }
+      //Fim de corrida (sensor de inicio/fim ativo e carro em movimento).
+      else if (leitura [4] == 1 && andar == 1){
+        andar = 0;
+        modo = parado;
+      }
   }
-  else if (leitura [4] == 1 && andar == 1){
-    andar = 0;
-    modo = parado;
-  }
-  
+
+  //Sensor de inicio/fim de curva ativo  
   if (leitura[0] == 1){
-    if (leitura[1] == leitura [3] == leitura[4] == 0 && leitura[2] == 1){
+    //Modifiquei o comparativo, acredito que nunca entraria nesse if, pois exigia que os sensores s1 e s3 fossem 0 ao msm tempo
+    if ((leitura[1] == 0|| leitura [3] == 0) && leitura[2] == 1){
       modo = curva;
       error = 0;
     }
-    else if (leitura[1] == leitura[2] == leitura[3] == leitura[4] == 1){
+    //Retirei o leitura[4] do if, acho que não é necessário.
+    else if (leitura[1] == leitura[2] == leitura[3] == 1){
       modo = andando;
       error = 0;
     }
   }
+  //Sensor de inicio/fim de curva
   else if (leitura[0] == 0){
+    //Acho que não entra nesse if, pois seria impossível sensor s1 e s3 serem 0 ao mesmo tempo.
     if (leitura[1] == 0 && leitura[2] == 1 && leitura[3] == 0 && leitura[4] == 1) {
       modo = andando;
       error = 0;
     }
-    else if (leitura[1] == 1){
+    //Acredito que o parâmetro estava invertido.
+    else if (leitura[1] == 0){
       modo = curva;
       error = -leitura[1];
     }
-    else if (leitura[3] == 1){
+    //Mesma coisa do caso anterior, acredito que o parâmetro estava invertido.
+    else if (leitura[3] == 0){
       modo = curva;
       error = leitura[3];
     }
-    else if (leitura[2] == 1 && leitura[1] == leitura[3] == leitura[4] == 0){
+    //Alterei o if para uma forma que entre no if
+    else if (leitura[2] == 1 && leitura[1] == 1 && leitura[3] == 1){
       modo = andando;
       error = 0;
     }
@@ -57,4 +71,3 @@ int pid_calculate (int ki, int kd, int kp, int error, int kr){
 
   return PID_value;
 }
-
